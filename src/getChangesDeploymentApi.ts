713d9615af3dd44e9182c0bcbf99368660c506d1
Jesper Madsen <jam@umbraco.dk>
2023-07-23 19:35:31 +0200

    let see

---
+++ src/getChangesDeploymentApi.ts
@@ -0,0 +1,56 @@
+import {HttpClient, MediaTypes, Headers } from '@actions/http-client';
+import { OutgoingHttpHeaders } from 'http';
+import { createWriteStream } from 'fs';
+
+export interface DeploymentStatusResponse {
+    "deploymentId": string,
+    "projectAlias": string,
+    "deploymentState": string,
+    "updateMessage": string,
+    "errorMessage": string,
+    "created": string,
+    "lastModified": string,
+    "completed": string
+}
+
+function generateHeaders(apiKey:string) : OutgoingHttpHeaders
+{
+    return {
+        [Headers.ContentType]: MediaTypes.ApplicationJson,
+        "Umbraco-Api-Key": apiKey
+    };
+}
+
+export async function getLatestDeploymentFromApi(baseUrl: string, apiKey: string): Promise<string>
+{
+    const headers = generateHeaders(apiKey);
+
+    const client = new HttpClient();
+    var response = await client.getJson<Array<DeploymentStatusResponse>>(`${baseUrl}?skip=0&take=1`, headers);
+
+    if (response.statusCode === 200 && response.result?.length === 1)
+    {
+        return Promise.resolve(response.result[0].deploymentId);
+    }
+
+    return Promise.reject(`Unexpected response coming from server. ${response.statusCode} - ${response.result} `);
+}
+
+export async function getChanges(baseUrl: string, apiKey: string, latestdeploymentId: string, downloadFolder: string) : Promise<void>
+{
+    const headers = generateHeaders(apiKey);
+    const client = new HttpClient();
+    const response = await client.get(`${baseUrl}/${latestdeploymentId}/diff`, headers);
+
+    if (response.message.statusCode === 204) {
+        return Promise.resolve();
+    }
+
+    if (response.message.statusCode === 200){
+        const file = createWriteStream(`${downloadFolder}`);
+        response.message.pipe(file).on('close', () => Promise.resolve());
+        return Promise.resolve();
+    }
+
+    return Promise.reject(`Unexpected response coming from server. ${response.message.statusCode} - ${response.readBody()} `);
+}
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { getInput, info, setFailed, warning, setOutput } from '@actions/core';
+import { getLatestDeploymentFromApi, getChanges } from './getChangesDeploymentApi';
+import { exists } from '@actions/io/lib/io-util';
+
+async function run() 
+{
+    const projectAlias = getInput('project-alias');
+    const apiKey = getInput('api-key');
+    const workspace = getInput('workspace');
+
+    const baseUrl = `https://api-internal.umbraco.io/projects/${projectAlias}/deployments`;
+
+    const latestDeployment = await getLatestDeploymentFromApi(baseUrl,apiKey);
+
+    const placeForPatch = `${workspace}/download/git-changes.patch`;
+
+    getChanges(baseUrl, apiKey, latestDeployment, placeForPatch)
+    .then(()=>success(placeForPatch),
+    ()=> setFailed("Unknown Error - unable to determine what happened :("));
+
+
+}
+
+async function success(patchfileLocation: string){
+    const patchFileExists = await exists(patchfileLocation);
+    if (patchFileExists){
+        warning(`Changes since last deployment was detected - see ${patchfileLocation}`);
+        setOutput('remote-changes', true);
+        setOutput('git-patch-file', patchfileLocation);
+        return;
+    }
+
+    info("No remote changes");
+    setOutput('remote-changes', false);
+}
+
+
+run();
+
