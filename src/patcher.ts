d02afeabaaa668f3ed21e285d1145e08c47e8730
Jesper Madsen <jam@umbraco.dk>
2023-07-29 22:53:01 +0200

    use async await

--- src/patcher.ts
+++ src/patcher.ts
@@ -1,19 +1,22 @@
 import { exec } from '@actions/exec';
+import { rmRF } from '@actions/io';
 
 export async function patch(patchFileLocation: string, githubToken: string, run: string) : Promise<void>
 {
-    return new Promise((resolve, reject) => {
+    return await new Promise(async (resolve, reject) => {
         try {
         const branchName = `auto/merge_remote_changes_for_run_${run}`
 
-        exec("git", ["config", "user.name", `"Build pipeline"`]);
-        exec("git", ["config", "user.email", "cicd+github@umbraco.com"]);
-        exec("git", ["switch", "-c", `${branchName}`]);
-        exec("git", ["apply", "-v", `${patchFileLocation}`]);
+        await exec("git", ["config", "user.name", `"Build pipeline"`]);
+        await exec("git", ["config", "user.email", "cicd+github@umbraco.com"]);
+        await exec("git", ["switch", "-c", `${branchName}`]);
+        await exec("git", ["apply", "-v", `${patchFileLocation}`]);
 
-        exec("git", ["commit", "-a", "-m", `"Auto Updated changes from remote for build ${run}"`]);
-        exec("git", ["remote", "add", "tmp-pusher", `https://${githubToken}@github.com/jesp209i/pipelines-r-us`]);         
-        exec("git", ["push", "--set-upstream", "tmp-pusher", `${branchName}`]);
+        rmRF(patchFileLocation);
+
+        await exec("git", ["commit", "-a", "-m", `"Auto Updated changes from remote for build ${run}"`]);
+        await exec("git", ["remote", "add", "tmp-pusher", `https://${githubToken}@github.com/jesp209i/pipelines-r-us`]);         
+        await exec("git", ["push", "--set-upstream", "tmp-pusher", `${branchName}`]);
         
         return resolve();
         }
